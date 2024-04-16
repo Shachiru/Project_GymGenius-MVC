2a07babba0a3f8b2df12499b41beee76e78ddc03
shach <shachirurashmika35@gmail.com>
2024-04-16 23:24:56 +0530

    ui pages setup

---
+++ src/main/java/lk/ijse/gymGenius/controller/GlobalFormController.java
@@ -0,0 +1,11 @@
+package lk.ijse.gymGenius.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class GlobalFormController {
+
+    @FXML
+    private Pane pagingPane;
+
+}
