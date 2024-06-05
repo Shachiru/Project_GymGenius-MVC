77fd8774adc75fa1efaea11d7f9683b98021945d
shach <shachirurashmika35@gmail.com>
2024-06-05 12:07:28 +0530

    scene builder ui has changed

--- src/main/java/lk/ijse/gymGenius/db/DbConnection.java
+++ src/main/java/lk/ijse/gymGenius/db/DbConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DbConnection {
+public class  DbConnection {
     private static DbConnection dbConnection;
     private Connection connection;
 
